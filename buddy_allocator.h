417fc9740f32b1cb93472652af8d9ae561c28a55
Raffaella Perna <raffaella.p05@gmail.com>
2022-01-24 10:35:24 +0100

    Modified header file to fit the bitmap structure

--- buddy_allocator.h
+++ buddy_allocator.h
@@ -1,27 +1,13 @@
 #pragma once
-#include "pool_allocator.h"
-#include "linked_list.h"
 
 #define MAX_LEVELS 16
 
-// one entry of the buddy list
-typedef struct BuddyListItem {
-  ListItem list;
-  int idx;   // tree index
-  int level; // level for the buddy
-  char* start; // start of memory
-  int size;
-  struct BuddyListItem* buddy_ptr;
-  struct BuddyListItem* parent_ptr;
-} BuddyListItem;
-
 
 typedef struct  {
-  ListHead free[MAX_LEVELS];
   int num_levels;
-  PoolAllocator list_allocator;
   char* memory; // the memory area to be managed
   int min_bucket_size; // the minimum page of RAM that can be returned
+  int* bitmap;
 } BuddyAllocator;
 
 
@@ -32,23 +18,23 @@ int BuddyAllocator_calcSize(int num_levels);
 // initializes the buddy allocator, and checks that the buffer is large enough
 void BuddyAllocator_init(BuddyAllocator* alloc,
                          int num_levels,
-                         char* buffer,
-                         int buffer_size,
                          char* memory,
-                         int min_bucket_size);
+                         int min_bucket_size
+                         int* bitmap;);
 
-// returns (allocates) a buddy at a given level.
-// side effect on the internal structures
+// returns (allocates) a buddy index at a given level.
 // 0 id no memory available
-BuddyListItem* BuddyAllocator_getBuddy(BuddyAllocator* alloc, int level);
+int BuddyAllocator_getBuddy(BuddyAllocator* alloc, int level);
 
 
 // releases an allocated buddy, performing the necessary joins
-// side effect on the internal structures
-void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, BuddyListItem* item);
+void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, int idx);
 
 //allocates memory
 void* BuddyAllocator_malloc(BuddyAllocator* alloc, int size);
 
 //releases allocated memory
 void BuddyAllocator_free(BuddyAllocator* alloc, void* mem);
+
+//aux function to get the index of the smallest buddy available
+int BuddyAllocator_minBuddy(BuddyAllocator* alloc);
